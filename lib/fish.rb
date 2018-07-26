3e8e16e86e17799155ddc342df914358b8b15541
ali aziz <aa_connect@outlook.com>
2018-07-26 12:07:33 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,27 @@
 class Fish
   # code goes here
+  
+  attr_accessor :mood
+  attr_reader :name
+  
+  @@all = []
+  
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def self.count
+    @@all.count
+  end
+    
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
   # code goes here
+  
+  @@all = []
+  
+  attr_reader :species
+  attr_accessor :name, :pets
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats=> []}
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def self.count
+    @@all.count  
+  end
+  
+  def say_species
+    return "I am a #{species}."
+  end
+  
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @@all << fish
+  end
 end
